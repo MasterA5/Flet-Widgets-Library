8ef2307dcf9a8185bf78f436bcc547f56b6fb5d8
MasterA5437S <agustin090806090806@gmail.com>
2025-11-07 11:16:27 -0300

    Updating __init__.py in BasicComponents

--- src/FletWidgetsLibrary/BasicComponents/__init__.py
+++ src/FletWidgetsLibrary/BasicComponents/__init__.py
@@ -1,4 +1,5 @@
+from .Stepper import Stepper, StepperStepCard, StepperEvent
 from .RestrictedInput import RestrictedInput
 from .BasicButton import BasicButton
 
-__all__ = ["RestrictedInput", "BasicButton"]
+__all__ = ["RestrictedInput", "BasicButton", "Stepper", "StepperStepCard", "StepperEvent"]
