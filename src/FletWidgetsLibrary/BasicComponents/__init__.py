0a931892c7fd34fc5078e5c036b11859dc5f25e9
MasterA5437S <agustin090806090806@gmail.com>
2025-11-12 21:08:53 -0300

    Adding the new widgets to __init__.py

--- src/FletWidgetsLibrary/BasicComponents/__init__.py
+++ src/FletWidgetsLibrary/BasicComponents/__init__.py
@@ -1,5 +1,5 @@
+from .RestrictedInput import RestrictedInput, BaseValidator, RestrictedInputEvent 
 from .Stepper import Stepper, StepperStepCard, StepperEvent
-from .RestrictedInput import RestrictedInput
 from .BasicButton import BasicButton
 
-__all__ = ["RestrictedInput", "BasicButton", "Stepper", "StepperStepCard", "StepperEvent"]
+__all__ = ["RestrictedInput", "BasicButton", "Stepper", "StepperStepCard", "StepperEvent", "BaseValidator", "RestrictedInputEvent"]
