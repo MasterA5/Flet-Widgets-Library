6b52a63bdb98b0beb85e0ee39b0e3691e54a311b
MasterA5437S <agustin090806090806@gmail.com>
2025-12-02 15:28:35 -0300

    Adding Resources folder

---
+++ src/FletWidgetsLibrary/Resources/__init__.py
@@ -0,0 +1,19 @@
+from .IconsBase64 import (
+    MicrosoftIcon,
+    InstagramIcon,
+    LinkedinIcon,
+    AmazonIcon,
+    GitHubIcon,
+    GoogleIcon,
+    AppleIcon,
+)
+
+__all__ = [
+    "MicrosoftIcon",
+    "InstagramIcon",
+    "LinkedinIcon",
+    "AmazonIcon",
+    "GitHubIcon",
+    "GoogleIcon",
+    "AppleIcon",
+]
